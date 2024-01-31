a0667d10280987be47ec2c2d9a28819016eeb1d0
SrinathRajan <126069805+SrinathRajan@users.noreply.github.com>
2024-01-31 09:58:03 +0530

    Add files via upload

---
+++ Day 4/src/pages/dashboard.jsx
@@ -0,0 +1,14 @@
+import CustomSideNavbar from "../components/CutomSidenav"
+import "../assets/css/dashboard.css"
+const Dashboard = () =>
+{
+    return (
+        <div>
+            <CustomSideNavbar/>
+            <div id="hello">
+                {/* <h1>welcome to chess Academy</h1> */}
+            </div>
+       </div>
+    )
+}
+export default Dashboard
