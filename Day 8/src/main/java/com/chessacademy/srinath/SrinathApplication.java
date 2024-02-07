07045de048839c6aa5e128fdd4562c5507dc83c6
SrinathRajan <126069805+SrinathRajan@users.noreply.github.com>
2024-02-07 15:44:52 +0530

    Add files via upload

---
+++ Day 8/src/main/java/com/chessacademy/srinath/SrinathApplication.java
@@ -0,0 +1,13 @@
+package com.chessacademy.srinath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrinathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrinathApplication.class, args);
+	}
+
+}
---
+++ Day 8/src/test/java/com/chessacademy/srinath/SrinathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessacademy.srinath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrinathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
